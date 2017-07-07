753d11effa8859f463dcb16e147a7b66bb2f9f94
Pepe Corredera <jcorredera@datiobd.com>
2017-07-07 05:27:12 +0200

    [~] se cambia la clase de package

---
+++ src/main/java/com/ipman1971/pfm/preprocessor/utils/PfmConstant.java
@@ -0,0 +1,10 @@
+package com.ipman1971.pfm.preprocessor.utils;
+
+/**
+ * Created by Ipman1971 on 19/06/2017.
+ *
+ * Clase para definicion de constantes usadas en esta aplicacion
+ */
+public interface PfmConstant {
+    public static final String ID_LOG="[PFM-PREPROCESSOR] - ";
+}
